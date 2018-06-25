4abc7e28b8e6608af321a700b0619956b1c76d8b
thewind1984 <thewind@mail.ru>
2018-06-25 12:05:11 +0300

    update demo assets

--- minify.sh
+++ minify.sh
@@ -4,3 +4,8 @@
 echo '--- minification starts:'
 uglifyjs --compress --mangle --comments /^\@/ --output ./dist/jquery.uat-1.0.min.js ./src/jquery.uat.js
 echo '--- minification done'
+
+echo '--- copy to docs starts:'
+cp ./lib/jquery-3.3.1.min.js ./docs/js/jquery-3.3.1.min.js
+cp ./dist/jquery.uat-1.0.min.js ./docs/js/jquery.uat-1.0.min.js
+echo '--- copy done'
